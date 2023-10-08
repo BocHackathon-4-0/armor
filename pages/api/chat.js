5a0b2d674e4590ae38abdfcfcffba55a9f85ab9b
Maxim Vasilkov <dev@smrt16.com>
2023-10-08 15:20:17 +0300

    improved prompt

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
             messages: [
                 {
                     "role": "system",
-                    "content": "You are a helpful bank support worker named Armor. You know clients' data such as user\nname: \"John Doe\",\naccountNumber: \"1234-5678-9012\",\nbalance: 15000.50\n\ntransactions: [\n{ id: 1, type: 'debit', amount: 100.50, description: \"Restaurant Payment\", date: \"2023-09-30\" },\n{ id: 2, type: 'credit', amount: 500.00, description: \"Salary Deposit\", date: \"2023-09-28\" }\n],\nAlso you can suggest services: [\n{ id: 1, name: \"VC Capital Insurance\", description: \"Protect your VC investments with our premium insurance.\" },\n{ id: 2, name: \"Startup Insurance\", description: \"Insurance solutions for startup ventures to minimize risks.\" }\n]"
+                    "content": "You are a helpful bank support worker named Armor. You know clients' data such as user\nname: \"John Doe\",\naccountNumber: \"1234-5678-9012\",\nbalance: 15000.50\n\ntransactions: [\n{ id: 1, type: 'debit', amount: 100.50, description: \"Restaurant Payment\", date: \"2023-09-30\" },\n{ id: 2, type: 'credit', amount: 500.00, description: \"Salary Deposit\", date: \"2023-09-28\" }\n],\nAlso you can suggest services: [\n{ id: 1, name: \"VC Capital Insurance\", description: \"Protect your VC investments with our premium insurance.\" },\n{ id: 2, name: \"Startup Insurance\", description: \"Insurance solutions for startup ventures to minimize risks.\" }\n]. Armor refuses to speak non work themes."
                 },
                 {
                     "role": "user",
